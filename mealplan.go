6f286a8ac992ef1e17017c349c834049be62dfd5
Pam Selle <pamela.selle@gmail.com>
2019-03-29 15:45:20 -0400

    Load styles and calculate meals
    Calculate meal with a recursive method to find a fit
    Print out single meals for now
    Also adds style methodology

--- mealplan.go
+++ mealplan.go
@@ -7,8 +7,10 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"os"
 	"strconv"
+	"time"
 )
 
 // Input is an input in a recipe (basically an ingredient)
@@ -30,13 +32,18 @@ type meal struct {
 var mealCount int
 var bases []Input
 var fillings []Input
+var styles []string
+
+var mealLowerBound = 300
+var mealUpperBound = 450
 
 func init() {
 	flag.IntVar(&mealCount, "m", 10, "number of meals for the plan")
 	// load the data and stuff
 	bases = loadData(bases, "data/bases.csv")
 	fillings = loadData(fillings, "data/fillings.csv")
-	// fmt.Println(fillings)
+	styles = loadStyles(styles, "data/styles.txt")
+	rand.Seed(time.Now().UnixNano())
 }
 
 func main() {
@@ -44,10 +51,35 @@ func main() {
 	generateMeals()
 }
 
+// try is a var to prevent a stack overflow so that we only try 3 times before we give up
+func generateMeal(base Input, filling Input, multiplier float32, try int) (string, float32) {
+	if try == 0 {
+		return "STOP", 1
+	}
+	var mealCalories = float32(base.calories)*multiplier + float32(filling.calories)
+	log.Print(mealCalories)
+	if float32(mealLowerBound) < mealCalories && float32(base.calories)*multiplier+float32(filling.calories) < float32(mealUpperBound) {
+		log.Print("Found meal")
+		return fmt.Sprintf(mealTitle, base.name, filling.name, styles[rand.Intn(len(styles))], 1), multiplier
+	}
+	if float32(base.calories)*multiplier+float32(filling.calories) < float32(mealUpperBound) {
+		return generateMeal(base, filling, multiplier+0.25, try-1)
+	}
+	if float32(mealLowerBound) < float32(base.calories)*multiplier+float32(filling.calories) {
+		log.Print("Decreasing base by 0.25")
+		log.Print(float32(base.calories)*multiplier + float32(filling.calories))
+		return generateMeal(base, filling, multiplier-0.25, try-1)
+	}
+	return "yo", multiplier
+}
 func generateMeals() {
 	fmt.Println(fmt.Sprintf("gonna generate %d meals", mealCount))
-	var meals []meal
-	fmt.Println(meals)
+	// var meals []meal
+	var base = bases[rand.Intn(len(bases))]
+	var filling = fillings[rand.Intn(len(fillings))]
+	var meal, multiplier = generateMeal(base, filling, 1, 3)
+	fmt.Println(meal)
+	fmt.Printf(recipeLine, float32(base.quantity)*multiplier, base.unit, base.name, base.dryMultiplier*multiplier)
 }
 
 func loadData(arr []Input, fileName string) []Input {
@@ -74,3 +106,30 @@ func loadData(arr []Input, fileName string) []Input {
 	}
 	return arr
 }
+
+func loadStyles(arr []string, fileName string) []string {
+	inputFile, _ := os.Open(fileName)
+	r := csv.NewReader(bufio.NewReader(inputFile))
+	for {
+		line, err := r.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+		arr = append(arr, line[0])
+	}
+	return arr
+}
+
+const mealTitle = `
+1. %s and %s, %s style, %d servings
+`
+
+// 1 cups brown rice, or .33 cup dry
+const recipeLine = `
+%.2f %s %s, or %.2f cup dry
+`
+const calCount = `
+%.2f calories per serving
+`
